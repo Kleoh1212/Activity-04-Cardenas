c4acf39d5d50bbb0d15b10108f30313c2f6de1d8
Abigail Cardenas <abigail.cardenas.edu.ph>
2022-04-26 23:01:32 +0800

    Message

--- opencv/test/main.py
+++ opencv/test/main.py
@@ -1,7 +1,33 @@
 import cv2
-filepath = 'red.jpg'
-windowTitle = 'Kleoh'
-readCvImage = cv2.imread(filepath,1)
-cv2.imshow( windowTitle , readCvImage)
+from tkinter import filedialog, Tk
+
+root = Tk()
+root.withdraw()
+
+filepath = filedialog.askopenfilename()
+
+
+print("Select Image Flag")
+print("1. Color")
+print("2. Grayscale")
+print("3. Unchange")
+
+flagInput = int(input())
+
+if flagInput == 1:
+    flag = 1
+elif  flagInput == 2:
+    flag = 0
+elif flagInput == 3: 
+    flag = -1
+else: 
+    flag = 1
+
+
+cv2.startWindowThread()
+image = cv2.imread(filepath, flag)
+cv2.imshow("Show", image)
+windowTitle = "Kleoh"
+cv2.imshow( windowTitle , image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
